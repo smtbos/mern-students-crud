ccd63df9f254863cda0cd761d843816c75574a77
smtbos <smtbos@gmail.com>
2024-02-07 01:04:21 +0530

    Update server.js to use process.env.PORT for listening

--- src/server.js
+++ src/server.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => {
     res.send("Hello ");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${port}`);
 })
